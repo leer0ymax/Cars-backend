42062edcb09f94f0c91e4165638269ad2f4d7a76
leeer0ymax <=leeroymax03@gmail.com>
2025-07-03 12:36:27 +0300

    updated the migrate.ts folder

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
-import app from ".";
+// import app from ".";
 
 
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// app.listen(3000, () => console.log("Server running on port 3000"));
+
+
+import app from "./index";
+import dotenv from 'dotenv/config';
+
+
+const PORT =  8080;
+app.listen(PORT, () => {
+
+    console.log(`Server is running on port http://localhost:${PORT}`);
+}) 
